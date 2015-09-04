7b1c6007d362564ee28b4bd5a67c734cae47aafc
Phil Mansfield <mansfield@uchicago.edu>
2015-09-04 08:28:52 -0500

    Removed neccessary nil checks from read_tree.go funcitons.

--- read_tree.go
+++ read_tree.go
@@ -20,16 +20,19 @@ type HaloTree struct { ptr *C.struct_halo_tree }
 func GetHaloTree() HaloTree { return HaloTree{ &C.halo_tree } }
 func GetAllHalos() HaloList { return HaloList{ &C.all_halos } }
 
-func (hl HaloList) Lookup(id int) Halo {
-	return Halo{ C.lookup_halo_in_list(hl.ptr, C.int64_t(id)) }
+func (hl HaloList) Lookup(id int) (Halo, bool) {
+	h := Halo{ C.lookup_halo_in_list(hl.ptr, C.int64_t(id)) }
+	return h, h.ptr != (*C.struct_halo)(nil)
 }
 
-func LookupScale(scale float64) HaloList {
-	return HaloList{ C.lookup_scale(C.float(scale)) }
+func LookupScale(scale float64) (HaloList, bool) {
+	hl := HaloList{ C.lookup_scale(C.float(scale)) }
+	return hl, hl.ptr != ((*C.struct_halo_list)(nil))
 }
 
-func FindClosestScale(scale float64) HaloList {
-	return HaloList{ C.find_closest_scale(C.float(scale)) }
+func FindClosestScale(scale float64) (HaloList, bool) {
+	hl := HaloList{ C.find_closest_scale(C.float(scale)) }
+	return hl, hl.ptr != ((*C.struct_halo_list)(nil))
 }
 
 func ReadTree(filename string) {
